162bd22998b07b095c33108f225c5320f10fe3bf
Alair <bandit12101@gmail.com>
2020-01-25 23:57:40 -0500

    Print Rules added (not yet complete)

--- Go Stop (C++)/main.cpp
+++ Go Stop (C++)/main.cpp
@@ -1,11 +1,14 @@
 // Main source file for C++ program to play the game Go Stop against a computer player
 
 #include <iostream>
+#include "gostop.h"
 
 using namespace std;
 
 int main() {
+	
+	print_rules();
 
-
+	system("PAUSE");
 	return 0;
 }
